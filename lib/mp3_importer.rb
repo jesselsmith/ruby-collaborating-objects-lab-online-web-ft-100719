a6714306b10e7d40d2f27cff98c9b6228be0344b
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-14 20:13:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,6 +6,8 @@ class MP3Importer
   end
 
   def import
-    Dir["#{path}/**/*.mp3]
-
+    song_filenames = Dir["#{path}/**/*.mp3"]
+    song_filenames.each{|filename| Song.new_by_filename(filename)}
+  end
+  
 end
