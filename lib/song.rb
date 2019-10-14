b735f64dd63b17c607ceba3e39edee7a24c0fd17
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-14 19:37:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,19 @@
 class Song
+  attr_accessor :name, :artist, :genre
+  @@all
+  def initialize(name)
+    self.name = name
+    self.save
+  end
 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  
 
 end
