4cb3ae523e18e6eb7d8f4105efcae2841f3c20c6
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-14 20:33:45 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,3 @@
-require_relative 'song'
-require_relative 'artist'
 class MP3Importer
   attr_accessor :path
 
@@ -16,13 +14,4 @@ class MP3Importer
     new_files = Dir["#{path}/**/*.mp3"]
     new_files.map{|filename| filename.split("/").last}
   end
-end
-
-
-test_music_path = "./spec/fixtures/mp3s"
-music_importer = MP3Importer.new(test_music_path)
-puts music_importer.import
-Artist.all.each do|artist|
-  puts artist.name + "'s songs:"
-  artist.print_songs
 end
