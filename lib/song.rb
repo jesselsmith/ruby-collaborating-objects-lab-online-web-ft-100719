efab29156dabe091eea20f375f019aff2dd166ce
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-14 19:44:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,13 @@ class Song
   def new_by_filename(filename)
     file_data = filename.split(/ - |.mp3/)
     new_song = Song.new(file_data[1])
-    new_song.artist = Artist.new(file_data[0])
+    new_song.artist = artist_name(file_data[0])
     new_song.genre = file_data[2]
     new_song
+  end
+
+  def artist_name(artist_name)
+    Artist.find_or_create_by_name(artist_name)
   end
 
 
